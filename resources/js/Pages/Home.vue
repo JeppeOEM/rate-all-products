ee3ea7277e658cff907bf0a0c32b0e1a0c14d962
Jeppe <jepp026b@stud.kea.dk>
2024-08-29 09:08:54 +0200

    basic laravel, inertia, tailwind and vue setup

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template>Hellocccccccccc {{ counter }}!
+<input type="file" @change="showFile">
+
+
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+<script setup>
+  import {ref} from 'vue'
+  const counter = ref(0)
+  setInterval(() => counter.value++, 1000)
+
+function showFile(input) {
+  let file = input.files[0];
+
+  alert(`File name: ${file.name}`); // e.g my.png
+  alert(`Last modified: ${file.lastModified}`); // e.g 1552830408824
+}
+</script>
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+// Add the import below
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        // Add this vue plugin configuration here
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        })
+    ],
+});
