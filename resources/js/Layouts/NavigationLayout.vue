7ca97b712ddfe57b08fab2aa0939b4dca8605cf4
Jeppe <jepp026b@stud.kea.dk>
2024-09-03 10:26:37 +0200

    Created Login, Create User pages and empty controller

---
+++ resources/js/Layouts/NavigationLayout.vue
@@ -0,0 +1,35 @@
+<template>
+    <Head>
+      <!-- fallback title -->
+      <title>Product ratings</title>
+    </Head>
+      <section class="p-6 bg-gray-200">
+        <header class="flex justify-between">
+          <div class="flex items-center">
+            <nav>
+              <Link href="/">Home</Link>
+              <Link href="/auth/login">Login</Link>
+              <Link href="/auth/logout">Logout</Link>
+              <Link href="/users/create">Register</Link>
+            </nav>
+          </div>
+    
+          <!-- <Nav /> -->
+        </header>
+      </section>
+    
+      <section class="p-6">
+        <div class="max-w-3xl mx-auto">
+          <slot />
+        </div>
+      </section>
+    </template>
+    
+    <script setup>
+    import { computed } from 'vue';
+    // import Nav from '../Components/Nav.vue';
+    import { Head, Link, usePage } from '@inertiajs/vue3';
+    
+    const page = usePage();
+    // const username = computed(() => page.props.auth.user.username);
+    </script>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,26 @@
 <?php
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Http\Controllers\UserController; 
 
 Route::get('/', function () {
     return inertia('Home');
 });
+Route::get('/auth/create', function () {
+    return inertia('Auth/Create');
+});
 
+
+
+Route::resource('users', UserController::class);
+
+// return redirect('/filelist');
+// });
+
+Route::post('/auth/login', function () {
+    return inertia('Auth/Login');
+    });
 
 Route::get('/filelist', function () {
     $allFiles = Storage::allFiles('/unimported');
