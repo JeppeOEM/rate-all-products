bd9f48c999d19ec797389ce9ccaff7ad42fa1f7b
Jeppe <jepp026b@stud.kea.dk>
2024-09-05 15:57:26 +0200

    default and null handling in migration + firstOrNew() implementation in saveToDb

--- app/Console/Commands/DownloadProducts.php
+++ app/Console/Commands/DownloadProducts.php
@@ -7,59 +7,43 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 
 use App\Services\FileHandler\XmlFileHandlerSoapPrefix;
+use League\CommonMark\Util\Xml;
+use Exception;
 
 class DownloadProducts extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'app:download-products';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Load products from file to database';
 
-    /**
-     * Execute the console command.
-     */
-    public function handle()
+    public function handle(): void
     {
+        $files = Storage::files('unimported');
 
+        if (empty($files)) {
+            $this->info('No files found in the unimported folder.');
+            return;
+        }
 
-            $files = Storage::files('unimported');
-            if (empty($files)) {
-                $this->info('No files found in the unimported folder.');
-                return;
-            }
-            $file = $this->choice('Please choose a XML file to import', $files);
-        
+        $file = $this->choice('Please choose a XML file to import', $files);
 
         $mimeType = Storage::mimeType($file);
-        $this->info("You have selected: $file");
-        $fileHandler = $this->createFileHandler($mimeType);
 
-        if ($fileHandler) {
-            $rows = $fileHandler->parseFile($file);
-            if($fileHandler->saveToDb($rows)){
-                $fileHandler->moveFile($file);
-            };
-        } else {
-            $this->error('Unsupported file type: ' . $mimeType);
-        }
+        $this->info("You have selected: $file");
+
+        $fileHandler = $this->createFileHandler($mimeType);
+        $rows = $fileHandler->parseFile($file);
+        $fileHandler->saveToDb($rows);
+        $fileHandler->moveFile($file);
     }
 
-    private function createFileHandler($mimetype)
+    private function createFileHandler($mimetype): XmlFileHandlerSoapPrefix
     {
         switch ($mimetype) {
             case 'text/xml':
             case 'application/xml':
                 return new XmlFileHandlerSoapPrefix();
-            }
+            default:
+                throw new Exception('Unsupported file type: ' . $mimetype);
+        }
     }
 }
-
