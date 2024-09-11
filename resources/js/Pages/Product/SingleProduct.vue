26b61fd0ac462dffac788e1d5cbd071bca50b53f
Jeppe <jappa669@gmail.com>
2024-09-11 20:05:56 +0200

    added alcohol percent to SingleProduct page

--- resources/js/Pages/Product/SingleProduct.vue
+++ resources/js/Pages/Product/SingleProduct.vue
@@ -1,54 +1,58 @@
 <template>
-    <Head :title="pageTitle" />
+  <Head :title="pageTitle" />
 
-    <div class="container mx-auto main-layout-padding">
-        <h1 class="headline py-4">{{ product.description }}</h1>
-        <p class="text-xl mb-2">
-            Price: {{ product.price }} {{ product.currency }}
-        </p>
-        <span class="text-xl mb-2">Average Rating: </span>
-        <span class="text-xl">{{ displayAvgRating }}</span>
+  <div class="container mx-auto main-layout-padding">
+      <h1 class="headline py-4">{{ product.description }}</h1>
+      <p class="text-xl mb-2">
+          Price: {{ product.price }} {{ product.currency }}
+      </p> 
 
-        <div class="mt-4">
-            <h2 class="text-xl font-bold mt-6 mb-4">Ratings</h2>
-            <section class="space-y-4">
-                <article
-                    v-for="rating in ratings"
-                    :key="rating.id"
-                    v-show="editingRatingId !== rating.id"
-                    class="bg-white p-4 custom-border space-y-2"
-                >
-                    <h3 class="text-xl">{{ rating.title }}</h3>
-                    <StarRating :rating="rating.rating" />
-                    <p class="text-gray-700 font-semibold">Review:</p>
-                    <span> {{ rating.comment }}</span>
-                    <div class="space-x-3" v-if="rating.user_id === userId">
-                        <button
-                            @click="deleteRating(rating.id)"
-                            class="black-button"
-                        >
-                            Delete Rating
-                        </button>
-                        <button
-                            @click="editRating(rating)"
-                            class="black-button"
-                        >
-                            Edit Rating
-                        </button>
-                    </div>
-                </article>
-            </section>
-            <div>
-                <RatingForm
-                    v-if="!hasUserRated || isEditing"
-                    :form="form"
-                    :isEditing="isEditing"
-                    :productId="product.id"
-                    @submit="submitRating"
-                />
-            </div>
-        </div>
-    </div>
+
+      <p class="text-xl"v-if="displayAlcoholPct">{{product.alcohol_pct}} %</p>
+
+      <span class="text-xl mb-2">Average Rating: </span>
+      <span class="text-xl">{{ displayAvgRating }}</span>
+
+      <div class="mt-4">
+          <h2 class="text-xl font-bold mt-6 mb-4">Ratings</h2>
+          <section class="space-y-4">
+              <article
+                  v-for="rating in ratings"
+                  :key="rating.id"
+                  v-show="editingRatingId !== rating.id"
+                  class="bg-white p-4 custom-border space-y-2"
+              >
+                  <h3 class="text-xl">{{ rating.title }}</h3>
+                  <StarRating :rating="rating.rating" />
+                  <p class="text-gray-700 font-semibold">Review:</p>
+                  <span> {{ rating.comment }}</span>
+                  <div class="space-x-3" v-if="rating.user_id === userId">
+                      <button
+                          @click="deleteRating(rating.id)"
+                          class="black-button"
+                      >
+                          Delete Rating
+                      </button>
+                      <button
+                          @click="editRating(rating)"
+                          class="black-button"
+                      >
+                          Edit Rating
+                      </button>
+                  </div>
+              </article>
+          </section>
+          <div>
+              <RatingForm
+                  v-if="!hasUserRated || isEditing"
+                  :form="form"
+                  :isEditing="isEditing"
+                  :productId="product.id"
+                  @submit="submitRating"
+              />
+          </div>
+      </div>
+  </div>
 </template>
 
 <script setup>
@@ -60,16 +64,19 @@ import StarRating from "@/Components/Rating/StarRating.vue";
 import RatingForm from "@/Components/Rating/RatingForm.vue";
 
 defineOptions({
-    layout: NavigationLayout,
+  layout: NavigationLayout,
 });
 
 const props = defineProps({
-    product: Object,
-    ratings: Array,
-    auth: Object,
-    avg_rating: Number,
-    errors: Object,
+  product: Object,
+  ratings: Array,
+  auth: Object,
+  avg_rating: Number,
+  errors: Object,
 });
+
+
+const displayAlcoholPct = computed(() => !!props.product.alcohol_pct);
 
 const displayAvgRating = computed(() => {
     return props.avg_rating !== null && props.avg_rating !== undefined
